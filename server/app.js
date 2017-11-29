b19a0a7c2808dcc8757832f542020b823a6c8060
bumsyalao <alaobunmi93@gmail.com>
2017-11-29 13:09:44 +0100

    [chore #153246146] serverside setup

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const router = express.Router();
+
+const PORT = parseInt(process.env.PORT, 10) || 3000;
+
+app.set('port', PORT);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`App started on port ${PORT}!`);
+});
