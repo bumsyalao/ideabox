2f3fdbaabc0675b1517aa0eeb03bc0fe8b149bb3
bumsyalao <alaobunmi93@gmail.com>
2017-12-10 14:42:06 +0100

    [Feature #153302514] Users can create ideas-UI

--- client/components/Access/Auth.jsx
+++ client/components/Access/Auth.jsx
@@ -42,6 +42,20 @@ class Auth extends Component {
     if (this.props.access.isAuthenticated) {
       this.props.history.push('/dashboard');
     }
+     // Variables
+    const signupButton = document.getElementById('signup-button');
+    const loginButton = document.getElementById('login-button');
+    const userForms = document.getElementById('user_options-forms');
+      // Add event listener to the "Sign Up" button
+    signupButton.addEventListener('click', () => {
+      userForms.classList.remove('login-click');
+      userForms.classList.add('signup-click');
+    }, false);
+      // Add event listener to the "Login" button
+    loginButton.addEventListener('click', () => {
+      userForms.classList.remove('signup-click');
+      userForms.classList.add('login-click');
+    }, false);
   }
 
   /**
--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
-import ideaboxlogo from '../../client/images/idea logo.png';
-import { logoutRequest } from '../actions/authAction';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import NavigationBar from '../components/NavigationBar';
+import CreateIdea from '../components/Ideas/CreateIdea';
 
 /**
  *
@@ -11,30 +11,19 @@ import { logoutRequest } from '../actions/authAction';
  * @extends {Component}
  */
 class Dashboard extends Component {
-
   /**
-   * Creates an instance of Dashboard.
-   * @param {object} props
+   *
+   * Check if user is authenticated
+   * Toast access message
+   * Intialize materialize collapsible
    * @return {void}
    * @memberOf Dashboard
    */
-  constructor(props) {
-    super(props);
-    this.logout = this.logout.bind(this);
-  }
-  /**
-   * Logout Function
-   * Prevent default action of event
-   * @param {object} event
-   * @return {void}
-   *
-   * @memberOf NavigationBar
-   */
-  logout(event) {
-    event.preventDefault();
-    this.props.logoutRequest();
-    this.props.history.push('/');
-    Materialize.toast('You have logged out successfully', 3000, 'green');
+  componentDidMount() {
+    if (!this.props.access.isAuthenticated) {
+      Materialize.toast('Please Login or Register', 5000, 'red');
+      this.props.history.push('/auth');
+    }
   }
   /**
    *
@@ -44,92 +33,21 @@ class Dashboard extends Component {
    * @memberOf Dashboard
    */
   render() {
-    $('.button-collapse').sideNav();
-    $(document).ready(function() {
-      $('.tooltipped').tooltip({ delay: 50 });
-    });
     return (
       <div className="dashboard">
-        <div className="nav-style nav">
-          <ul id="slide-out" className="side-nav">
-            <li>
-              <div className="user-view">
-                <div className="background">
-                  <img src="images/office.jpg" />
-                </div>
-                <img className="img-logo" src={ideaboxlogo} />
-                <a href="#!name">
-                  <span className="black-text name">John Doe</span>
-                </a>
-                <a href="#!email">
-                  <span className="black-text email">jdandturk@gmail.com</span>
-                </a>
-              </div>
-            </li>
-            <li>
-              <a href="#!">
-                <i className="material-icons">person_outline</i>Edit Profile
-              </a>
-            </li>
-            <li>
-              <div className="divider" />
-            </li>
-            <li>
-              <a className="waves-effect" href="#!">
-                <i className="material-icons">lightbulb_outline</i>
-                My Ideas
-              </a>
-            </li>
-          </ul>
-          <a
-            href="#"
-            className="button-collapse tooltipped"
-            data-position="bottom"
-            data-delay="50"
-            data-tooltip="Menu"
-            data-activates="slide-out"
-          >
-            <i className="material-icons">menu</i>
-          </a>
-          <div className="nav-button">
-            <a
-              href="#"
-              className="tooltipped"
-              data-position="bottom"
-              data-delay="50"
-              data-tooltip="Create Idea"
-            >
-              <i className="material-icons">add</i>
-            </a>
-            <a
-              href="#"
-              className="tooltipped"
-              data-position="bottom"
-              data-delay="50"
-              data-tooltip="Search Ideas"
-            >
-              <i className="material-icons">search</i>
-            </a>
-            <a
-              href="#"
-              onClick={this.logout}
-              className="tooltipped"
-              data-position="bottom"
-              data-delay="50"
-              data-tooltip="Logout"
-            >
-              <i className="material-icons">power_settings_new</i>
-            </a>
-          </div>
-        </div>
-        <div className="group-list" />
+        <NavigationBar />
+        <Switch>
+          <Route
+            path={`${this.props.match.url}/create-idea`}
+            component={CreateIdea}
+          />
+        </Switch>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  access: state.access.user
+  access: state.access
 });
-
-export default connect(mapStateToProps, { logoutRequest })(withRouter(Dashboard));
+export default connect(mapStateToProps, null)(withRouter(Dashboard));
